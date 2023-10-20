398afff96c52a0b8ae9165bb41108aa80ffed10e
Mohamad Akmal Ramadhanta <42136962+ramadhantaAkmal@users.noreply.github.com>
2023-10-20 14:52:53 +0700

    feat:loading overlay change

--- lib/features/presentation/widgets/loading_overlay_widget.dart
+++ lib/features/presentation/widgets/loading_overlay_widget.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:mytradeasia/config/themes/theme.dart';
 
 class LoadingOverlay extends StatelessWidget {
   const LoadingOverlay({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // final spinkit = SpinKitFadingCircle(
+    //   itemBuilder: (BuildContext context, int index) {
+    //     return DecoratedBox(
+    //       decoration: BoxDecoration(
+    //         color: index.isEven ? Colors.red : Colors.green,
+    //       ),
+    //     );
+    //   },
+    // );
     return Stack(
       children: [
         // Grey background
@@ -17,8 +28,17 @@ class LoadingOverlay extends StatelessWidget {
         ),
 
         // Loading indicator
-        const Center(
-          child: CircularProgressIndicator(),
+        Center(
+          child: Container(
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(10)),
+            child: Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: SpinKitFadingCircle(color: primaryColor1),
+            ),
+          ),
         ),
       ],
     );
