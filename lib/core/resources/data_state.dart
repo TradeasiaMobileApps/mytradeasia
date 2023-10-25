7f98dd7ef346d1cd94508a3c91dcb3ecbd180a73
Mohamad Akmal Ramadhanta <42136962+ramadhantaAkmal@users.noreply.github.com>
2023-10-25 14:23:15 +0700

    docs:added documentation

--- lib/core/resources/data_state.dart
+++ lib/core/resources/data_state.dart
@@ -1,16 +1,43 @@
 import 'package:dio/dio.dart';
 
+/// An abstract class representing the state of data retrieval or manipulation.
+///
+/// This class encapsulates both the successful result of a data operation, represented by the [data] property,
+/// and any potential error that might occur during the operation, represented by the [error] property.
+///
+/// Subclasses of [DataState] provide specific implementations for successful and failed data states.
+///
+/// [DataSuccess] represents a successful data retrieval or manipulation operation, holding the retrieved or manipulated data.
+/// [DataFailed] represents a failed data operation, holding the error information associated with the failure.
+///
+/// @param T The type of data associated with the data state.
 abstract class DataState<T> {
+  /// The retrieved or manipulated data, if the operation was successful.
   final T? data;
+
+  /// The error information, if the operation failed.
   final DioException? error;
 
+  /// Creates a [DataState] instance with optional [data] and [error] values.
   const DataState({this.data, this.error});
 }
 
+/// Represents a successful data retrieval or manipulation operation.
+///
+/// This class extends [DataState] and holds the retrieved or manipulated data in its [data] property.
+///
+/// @param T The type of data associated with the successful data state.
 class DataSuccess<T> extends DataState<T> {
+  /// Creates a [DataSuccess] instance with the retrieved or manipulated [data].
   const DataSuccess(T data) : super(data: data);
 }
 
+/// Represents a failed data retrieval or manipulation operation.
+///
+/// This class extends [DataState] and holds the error information associated with the failure in its [error] property.
+///
+/// @param T The type of data associated with the failed data state.
 class DataFailed<T> extends DataState<T> {
+  /// Creates a [DataFailed] instance with the [error] information.
   const DataFailed(DioException error) : super(error: error);
 }
--- lib/core/usecase/usecase.dart
+++ lib/core/usecase/usecase.dart
@@ -1,13 +1,48 @@
+/// An abstract base class for use cases.
+///
+/// A use case is a piece of code that performs a specific operation, such as fetching data from a remote server, saving data to a local database, or processing user input.
+///
+/// This abstract class provides a basic interface for all use cases. It declares a single method, `call()`, which must be implemented by all subclasses. The `call()` method takes a single parameter, `param`,
+/// which is a generic type that represents the parameters required by the use case.
+///
+/// To use a use case, simply create a new instance of the subclass that implements the desired functionality.
+/// Then, call the `call()` method, passing in the required parameters. The `call()` method will return a `Future` of type `Type`,
+/// which represents the output of the use case.
+///
+/// @param Type The type of the output of the use case.
+/// @param Params The type of the parameters required by the use case.
 abstract class UseCase<Type, Params> {
+  /// Performs the use case operation and returns a `Future` of type `Type`, which represents the output of the use case.
   Future<Type> call({required Params param});
 }
 
+/// An abstract base class for use cases with three parameters.
+///
+/// This class extends `UseCase` and provides a specific interface for use cases that require three parameters.
+///
+/// @param Type The type of the output of the use case.
+/// @param A The type of the first parameter required by the use case.
+/// @param B The type of the second parameter required by the use case.
+/// @param C The type of the third parameter required by the use case.
+
 abstract class UseCaseThreeParams<Type, A, B, C> {
+  /// Performs the use case operation with the given parameters and returns a `Future` of type `Type`, which represents the output of the use case.
   Future<Type> call(
       {required A paramsOne, required B paramsTwo, required C paramsThree});
 }
 
+/// An abstract base class for use cases with four parameters.
+///
+/// This class extends `UseCase` and provides a specific interface for use cases that require four parameters.
+///
+/// @param Type The type of the output of the use case.
+/// @param A The type of the first parameter required by the use case.
+/// @param B The type of the second parameter required by the use case.
+/// @param C The type of the third parameter required by the use case.
+/// @param D The type of the fourth parameter required by the use case.
+
 abstract class UseCaseFourParams<Type, A, B, C, D> {
+  /// Performs the use case operation with the given parameters and returns a `Future` of type `Type`, which represents the output of the use case.
   Future<Type> call(
       {required A paramsOne,
       required B paramsTwo,
